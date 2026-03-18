6a24e8b500fb9f28f4b05258ec7a4b7865b640c1
Berkay TİKENOĞLU <berkaytikenoglu@gmail.com>
2026-03-18 23:47:30 +0300

    düzeltme.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,13 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  // eslint bloğunu sildik, çünkü artık desteklenmiyor
   typescript: {
+    // TypeScript hatalarını görmezden gelmek için burası hala geçerli
     ignoreBuildErrors: true,
   },
 };
-
 export default nextConfig;
